a5a1e6f2cd9164eb252d0403050b8b595305f93e
Alexander Vollschwitz <vollschwitz@gmx.net>
2022-06-22 22:23:51 +0200

    [wip] upgrade via ui

---
+++ pkg/control/apiupgrade_unsupported.go
@@ -0,0 +1,33 @@
+//go:build !linux && !darwin
+
+/*
+   OqtaDrive - Sinclair Microdrive emulator
+   Copyright (c) 2022, Alexander Vollschwitz
+
+   This file is part of OqtaDrive.
+
+   OqtaDrive is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   OqtaDrive is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with OqtaDrive. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package control
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (a *api) upgrade(w http.ResponseWriter, req *http.Request) {
+	handleError(fmt.Errorf("upgrade not supported on this platform"),
+		http.StatusInternalServerError, w)
+}
